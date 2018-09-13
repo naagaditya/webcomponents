05b0ce48e8e6fc934c135a168480b514bbc7d0f5
adityanaag <adityanaag91@gmail.com>
2018-09-13 20:13:26 +0530

    created header

--- web-components/packages/zcui-wc-search-widget/dist/bundle.js
+++ web-components/packages/zcui-wc-search-widget/dist/bundle.js
@@ -12,63 +12,89 @@ class ZcuiWcSearchWidget extends HTMLElement {
   get htmlTemplate() {
     return html`
       <style>
-        .zcui-wc-search-widget{display:flex;padding:10px;font-size:12px;flex-direction:column}.zcui-wc-search-widget img{width:100%;max-width:500px}.zcui-wc-search-widget label{color:green;font-weight:600}.zcui-wc-search-widget .search-input{display:flex;margin-top:10px;flex-direction:column}.zcui-wc-search-widget .search-input .input-box{display:inline}.zcui-wc-search-widget .search-input .input-wrapper{display:flex;flex-direction:column}
+        .zcui-wc-search-widget{display:flex;padding:20px;font-size:12px;flex-direction:column;font-family:Arial, Helvetica, sans-serif}.zcui-wc-search-widget header{display:flex;margin:auto}.zcui-wc-search-widget header .logo-container{padding:0 20px;margin:10px 0;border-right:solid 1px #cecece}.zcui-wc-search-widget header .logo{width:127px}.zcui-wc-search-widget header .title{padding:10px 20px;font-size:13px;width:125px}.zcui-wc-search-widget label{color:green;letter-spacing:.5px}.zcui-wc-search-widget .search-input{display:flex;margin-top:10px;flex-direction:column}.zcui-wc-search-widget .search-input .input-box{display:inline;border:solid 1px #cecece3d;box-shadow:3px 5px 5px #8888882e;padding:5px;margin:7px 0}.zcui-wc-search-widget .search-input .input-box select{opacity:0;position:absolute;top:0;left:0;bottom:0;right:0}.zcui-wc-search-widget .search-input .input-box .input{position:relative;height:35px}.zcui-wc-search-widget .search-input .input-wrapper{display:flex;flex-direction:column}
 
       </style>
       <div class="zcui-wc-search-widget">
-  <img alt="logo"/>
+  <header>
+    <div class="logo-container">
+      <img alt="logo" src="../img/logo.svg" class="logo"/>
+    </div>
+    <i class="title">
+       Enjoy Self Drive Cars Starting <b>Rs 60/Hr*</b>
+    </i>
+    
+  </header>
   <div class="search-input">
     <div class="input-wrapper">
       <label>Pick Up Location</label>
       <div class="input-box">
-        <select>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-        </select>
-        <select>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-        </select>
+        <div class="input">
+          <span>Select City</span>
+          <select>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+          </select>
+        </div>
+        <div class="input">
+          <span>Select City</span>
+          <select>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+          </select>
+        </div>
       </div>
     </div>
 
     <div class="input-wrapper">
       <label>Start Date and Time</label>
       <div class="input-box">
-        <select>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-        </select>
-        <select>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-        </select>
+        <div class="input">
+          <span>Select City</span>
+          <select>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+          </select>
+        </div>
+        <div class="input">
+          <span>Select City</span>
+          <select>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+          </select>
+        </div>
       </div>
     </div>
     
     <div class="input-wrapper">
       <label>End Date and Time</label>
       <div class="input-box">
-        <select>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-        </select>
-        <select>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-          <option value="option1">option1</option>
-        </select>
+        <div class="input">
+          <span>Select City</span>
+          <select>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+          </select>
+        </div>
+        <div class="input">
+          <span>Select City</span>
+          <select>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+            <option value="option1">option1</option>
+          </select>
+        </div>
       </div>
     </div>
     <button> Search Car</button>
