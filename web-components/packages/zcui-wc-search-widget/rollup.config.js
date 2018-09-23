01d559ac4515746bbd8f33b2eead00fddd32ca9e
adityanaag <adityanaag91@gmail.com>
2018-09-23 11:36:41 +0530

    added polyfill

--- web-components/packages/zcui-wc-search-widget/rollup.config.js
+++ web-components/packages/zcui-wc-search-widget/rollup.config.js
@@ -1,8 +1,16 @@
 import babel from 'rollup-plugin-babel';
 import { uglify } from "rollup-plugin-uglify";
+import babelRuntimeExternal from 'rollup-plugin-babel-runtime-external';
+
+
 export default {
   input: './dist/bundle.js',
-  plugin: [babel(), uglify()],
+  plugin: [babel(), uglify(),
+    babelRuntimeExternal({
+      helpers: false,
+      polyfill: true,
+      regenerator: false,
+    })],
   output: {
     file: './dist/bundle.min.js',
     format: 'iife'
