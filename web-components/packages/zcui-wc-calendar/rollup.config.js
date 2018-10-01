ddfc6f38b8125db2557a23c815269f4dc4841235
karan verma <karan.verma@zoomcar.com>
2018-10-01 16:13:29 +0530

    fix(search/calendar): updated calendar build process

--- web-components/packages/zcui-wc-calendar/rollup.config.js
+++ web-components/packages/zcui-wc-calendar/rollup.config.js
@@ -3,9 +3,14 @@ import { uglify } from "rollup-plugin-uglify";
 import babelRuntimeExternal from 'rollup-plugin-babel-runtime-external';
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
+import multiEntry from "rollup-plugin-multi-entry";
 
 export default {
-  input: './dist/bundle.js',
+  input: [
+    './dist/bundle-month-calendar.js',
+    './dist/bundle-time-picker.js',
+    './dist/bundle.js',
+  ],
   plugin: [babel(), uglify(),serve(),livereload('dist'),
     babelRuntimeExternal({
       helpers: false,
@@ -13,10 +18,11 @@ export default {
       regenerator: false,
     })],
   output: {
-    file: './dist/bundle.min.js',
+    file: './dist/bundle-datetime-picker.min.js',
     format: 'iife'
   },
   plugins: [
+    multiEntry(),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers']
